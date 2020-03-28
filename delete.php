552a92222c12093b56a5066d862192f074b4307c
Rosama AIsani <604234018@parichat.skru.ac.th>
2020-03-28 16:40:38 +0700

    bookshop

--- delete.php
+++ delete.php
@@ -1,8 +1,7 @@
 <?php
 require 'db.php';
 $id = $_GET['list_id'];
-$sql = 'DELETE FROM listsaving WHERE list_id=:list_id';
+$sql = 'DELETE FROM book WHERE bookID=' $_GET['id'];
 $statement = $connection->prepare($sql);
-if ($statement->execute([':list_id' => $id])) {
-  header("Location: index.php");
-}
+  header("Location: booklist.php");
+?>
